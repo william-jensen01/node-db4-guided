3cf2b003f6eef939a8ca2d59097fbd88c94eff2c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-21 19:45:34 +0100

    wip

--- data/migrations/20210121192934_first-migration.js
+++ data/migrations/20210121192934_first-migration.js
@@ -12,7 +12,7 @@ exports.up = function (knex) {
     .createTable('animals', tbl => {
       tbl.increments('animal_id')
       tbl.string("animal_name", 128).notNullable();
-      tbl.integer('species_id')
+      tbl.integer('species_id').unsigned().notNullable().
     })
     .createTable()
 };
