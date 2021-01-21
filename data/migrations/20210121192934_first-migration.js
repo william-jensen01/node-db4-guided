9d95208c628c675f3beef345827775b4c87852a2
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-21 19:31:14 +0100

    wip

--- data/migrations/20210121192934_first-migration.js
+++ data/migrations/20210121192934_first-migration.js
@@ -1,11 +1,15 @@
-exports.up = function(knex) {
- return knex.schema
-  .createTable()
-  .createTable()
-  .createTable()
-  .createTable()
+exports.up = function (knex) {
+  return knex.schema
+    .createTable()
+    .createTable()
+    .createTable()
+    .createTable()
 };
 
-exports.down = function(knex) {
-
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
 };
