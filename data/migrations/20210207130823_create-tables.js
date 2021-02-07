b85cd1b7bb990c73327a7614795eb097854c4f20
William Jensen <williambjensen01@gmail.com>
2021-02-07 14:03:22 -0700

    completed guided

---
+++ data/migrations/20210207130823_create-tables.js
@@ -0,0 +1,37 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('zoos', tbl => {
+            tbl.increments();
+            tbl.string('zoo_name', 128).notNullable();
+            tbl.string('address', 128).notNullable().unique();
+        })
+        .createTable('species', tbl => {
+            tbl.increments();
+            tbl.string('species_name', 128).notNullable().unique();
+        })
+        .createTable('animals', tbl => {
+            tbl.increments();
+            tbl.string('animal_name', 128).notNullable();
+            tbl.integer('species_id')
+                .unsigned().notNullable().references('species.id')
+                .onDelete('CASCADE').onUpdate('CASCADE');
+        })
+        .createTable('zoo_animals', tbl => {
+            tbl.integer('zoo_id')
+                .unsigned().notNullable().references('zoos.id')
+                .onDelete('CASCADE').onUpdate('CASCADE');
+            tbl.integer('animal_id')
+                .unsigned().notNullable().references('animals.id')
+                .onDelete('CASCADE').onUpdate('CASCADE');
+            tbl.primary(['zoo_id', 'animal_id']);
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('zoo_animals')
+        .dropTableIfExists('animals')
+        .dropTableIfExists('species')
+        .dropTableIfExists('zoos');
+};
